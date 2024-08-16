ed9384be2e284cd374d779188d0279abc9aa8d9f
Anas7488 <151657403+Anas7488@users.noreply.github.com>
2024-08-16 21:52:55 +0530

    Update Face_Detector.py

--- OpencV_Programs/Face_Detector.py
+++ OpencV_Programs/Face_Detector.py
@@ -1,13 +1,11 @@
 import cv2
-face_cascade = cv2.CascadeClassifier("//Library//Frameworks//Python.framework//Versions//3.12//lib//python3.12//site-packages//cv2//data//haarcascade_frontalface_alt.xml")
-cap = cv2.VideoCapture(0)
+video_cap = cv2.VideoCapture(0)
+face_cap = cv2.CascadeClassifier("/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-package/cv2/data/haarcascade_frontalface_alt.xml")
 while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces =  face_cascade.detectMultiScale(gray,1.1,4)
-    for(x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(225,0,0),2)
-        cv2.imshow("Face_Detector",frame)
-        if cv2.waitKey(10) == ord('a'):
-            break
-        cap.release()
+    ret , frame = video_cap.read()
+    col = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces =  face_cap.detectMultiScale(col,1.1,4)
+    cv2.imshow("Face Detector",frame)
+    if cv2.waitKey(10) == ord("a"):
+        break
+    video_cap.release()
